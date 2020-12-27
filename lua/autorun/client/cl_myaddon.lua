7f1d5f80dcddcc69d7038f03e0a7626b387ee2b2
Arthur Molinari <arthureduardomolinari@gmail.com>
2020-12-27 12:45:41 -0300

    UP

--- lua/autorun/client/cl_myaddon.lua
+++ lua/autorun/client/cl_myaddon.lua
@@ -1,36 +1,29 @@
 include("autorun/sh_myaddon.lua")
 
-local Frame = vgui.Create( "DFrame" )
-Frame:SetTitle( "Simple Panel" )
-Frame:SetSize(300, 300)
-Frame:Center()
-Frame:MakePopup()
-Frame.Paint = function(self, w, h)
-    draw.RoundedBox(5, 0, 0, w, h, Color(50, 94, 168))
-end
+net.Receive("WantedLevelSender", function()
+    clWantedLevel = net.ReadString()
+    clMaxWantedLevel = net.ReadString()
+    clMinWantedLevel = net.ReadString()
+end)
 
-ButtonKillAll = vgui.Create( "DButton", Frame )
-ButtonKillAll:SetText( "Kill All Players" )
-ButtonKillAll:SetTextColor(Color(255, 255, 255))
-ButtonKillAll:SetPos(5, 30)
-ButtonKillAll:SetSize(100, 30)
-ButtonKillAll.Paint = function(self, w, h)
-    draw.RoundedBox(0, 0, 0, w, h, Color(81, 115, 173))
-end
-ButtonKillAll.DoClick = function()
-    net.Start("KillAllPlayers")
-    net.SendToServer()
-end
+print("Wanted Lvl: " .. clWantedLevel)
+print("Min Lvl: " .. clMinWantedLevel)
+print("Max Lvl: " .. clMaxWantedLevel)
 
-ButtonSetLife = vgui.Create( "DButton", Frame )
-ButtonSetLife:SetText( "Kill All Players" )
-ButtonSetLife:SetTextColor(Color(255, 255, 255))
-ButtonSetLife:SetPos(110, 30)
-ButtonSetLife:SetSize(100, 30)
-ButtonSetLife.Paint = function(self, w, h)
-    draw.RoundedBox(0, 0, 0, w, h, Color(81, 115, 173))
-end
-ButtonSetLife.DoClick = function()
-    net.Start("SetPlayerHealth")
-    net.SendToServer()
+local frame = vgui.Create( "DFrame" )
+frame:SetTitle( "Dock Test" )
+frame:SetSize( 256, 256 )
+frame:Center()
+frame:MakePopup()
+
+local panel = vgui.Create( "DPanel", frame )
+panel:Dock( FILL )
+panel:DockMargin( 0, 0, 0, 0 )
+
+for i = 1, clWantedLevel, 1 do
+	local label = vgui.Create( "DLabel", panel )
+	label:Dock( TOP )
+	label:DockMargin( 4, 0, 0, 0 ) -- shift to the right
+	label:SetColor( color_black )
+	label:SetText( "Wanted Lvl " .. i )
 end
--- lua/autorun/server/sv_myaddon.lua
+++ lua/autorun/server/sv_myaddon.lua
@@ -1,33 +1,66 @@
 include("autorun/sh_myaddon.lua")
 
-util.AddNetworkString("KillAllPlayers")
-util.AddNetworkString("SetPlayerHealth")
+util.AddNetworkString("WantedLevelSender")
 
-local function KillAllPlayers()
-    local players = player.GetAll()
-    for i, ply in ipairs(players) do
-        ply:Kill()    
-        print(ply:Nick() .. " got killed.")
+PLAYER_STATUS = {
+    WantedLevel = 0,
+    MinWantedLevel = 0,
+    MaxWantedLevel = 5,
+    Message = ""
+}
+
+function PLAYER_STATUS.AddWantedLevel(i)
+    if ((PLAYER_STATUS.WantedLevel + i) > PLAYER_STATUS.MaxWantedLevel) then
+        PLAYER_STATUS.Message = "FAILED! Bounty Level parameter violated."
+    else
+        PLAYER_STATUS.WantedLevel = PLAYER_STATUS.WantedLevel + i
+        PLAYER_STATUS.Message = "SUCCESS! Bounty Level increased."
     end
 end
 
-local function SetPlayerHealth()
-    local players = player.GetAll()
-    for i, ply in ipairs(players) do
-        ply:SetHealth(100)    
+function PLAYER_STATUS.RemoveWantedLevel(i)
+    if ((PLAYER_STATUS.WantedLevel - i) < PLAYER_STATUS.MinWantedLevel) then
+        PLAYER_STATUS.Message = "FAILED! Bounty Level parameter violated."
+    else
+        PLAYER_STATUS.WantedLevel = PLAYER_STATUS.WantedLevel - i
+        PLAYER_STATUS.Message = "SUCCESS! Bounty Level decreased."
     end
 end
 
-local function NetReceiver(strNet, func)
-    net.Receive(strNet, function()
-        local runThisFunction = func
-    end)
+function PLAYER_STATUS.AddMaxWantedLevel()
+    if (PLAYER_STATUS.WantedLevel ~= PLAYER_STATUS.MaxWantedLevel) then
+        PLAYER_STATUS.WantedLevel = PLAYER_STATUS.MaxWantedLevel
+        PLAYER_STATUS.Message = "SUCCESS! Bounty Level is on max level."
+    else
+        PLAYER_STATUS.Message = "FAILED! Already in max level"
+    end
 end
 
---net.Receive("KillAllPlayers", function()
---    KillAllPlayers()
---end)
+table.insert(PLAYER_STATUS, BountyAdd)
+table.insert(PLAYER_STATUS, BountyDecrease)
+table.insert(PLAYER_STATUS, BountyAddMax)
+PLAYER_STATUS.BountyAdd = PLAYER_STATUS.AddWantedLevel
+PLAYER_STATUS.BountyDecrease = PLAYER_STATUS.RemoveWantedLevel
+PLAYER_STATUS.BountyAddMax = PLAYER_STATUS.AddMaxWantedLevel
 
---net.Receive("SetPlayerHealth", function()
---    SetPlayerHealth()
---end)
+function PLAYER_STATUS:new(o)
+    o = o or {}
+    setmetatable(o, self)
+    self.__index = self
+    return o
+end
+
+Account = PLAYER_STATUS:new()
+
+Account.BountyAdd(2)
+Account.BountyAdd(2)
+
+function SendWantedLvl(lvl, max, min)
+    net.Start("WantedLevelSender")
+    net.WriteString(Account.WantedLevel)
+    net.WriteString(Account.MaxWantedLevel)
+    net.WriteString(Account.MinWantedLevel)
+    net.Broadcast()
+end
+
+SendWantedLvl()
